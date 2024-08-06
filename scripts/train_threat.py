e1c32e8a67d528a73502186511771de187f1ea8c
lei.lv <lei.lv@boomingtech.com>
2024-08-06 11:12:32 +0800

    代码整理

--- scripts/train_threat.py
+++ scripts/train_threat.py
@@ -8,7 +8,7 @@ import pytorch_lightning as pl
 
 sys.path.append(r"./src")
 from module import NeuralAstar
-from data_utils.maze_data import create_dataloader
+# from data_utils.maze_data import create_dataloader
 from train_utils.basic_planner import PlannerModule, set_global_seeds
 from pytorch_lightning.callbacks import ModelCheckpoint
 
@@ -17,25 +17,25 @@ def main(config):
 
     # dataloaders
     set_global_seeds(config.seed)
-    train_loader = create_dataloader(
-        config.dataset + ".npz", "train", config.params.batch_size, shuffle=True
-    )
-    val_loader = create_dataloader(
-        config.dataset + ".npz", "valid", config.params.batch_size, shuffle=False
-    )
+    # train_loader = create_dataloader(
+    #     config.dataset + ".npz", "train", config.params.batch_size, shuffle=True
+    # )
+    # val_loader = create_dataloader(
+    #     config.dataset + ".npz", "valid", config.params.batch_size, shuffle=False
+    # )
 
-    neural_astar = NeuralAstar(
-        encoder_input=config.encoder.input,
-        encoder_arch=config.encoder.arch,
-        encoder_depth=config.encoder.depth,
-        learn_obstacles=False,
-        Tmax=config.Tmax,
-    )
+    # neural_astar = NeuralAstar(
+    #     encoder_input=config.encoder.input,
+    #     encoder_arch=config.encoder.arch,
+    #     encoder_depth=config.encoder.depth,
+    #     learn_obstacles=False,
+    #     Tmax=config.Tmax,
+    # )
     checkpoint_callback = ModelCheckpoint(
         monitor="metrics/h_mean", save_weights_only=True, mode="max"
     )
 
-    module = PlannerModule(neural_astar, config)
+    # module = PlannerModule(neural_astar, config)
     logdir = f"{config.logdir}/{os.path.basename(config.dataset)}"
     trainer = pl.Trainer(
         accelerator="gpu" if torch.cuda.is_available() else "cpu",
@@ -44,7 +44,7 @@ def main(config):
         max_epochs=config.params.num_epochs,
         callbacks=[checkpoint_callback],
     )
-    trainer.fit(module, train_loader, val_loader)
+    # trainer.fit(module, train_loader, val_loader)
 
 
 if __name__ == "__main__":
