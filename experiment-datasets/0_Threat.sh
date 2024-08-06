9c196881b491c8d74fc686bc906dc3dfb3eb084a
BOOMING\guangqian.chen <123@qq.com>
2024-08-06 17:46:04 +0800

    生成带威胁值的数据

--- experiment-datasets/0_Threat.sh
+++ experiment-datasets/0_Threat.sh
@@ -1,4 +1,4 @@
 for maze in alternating_gaps bugtrap_forest forest gaps_and_forest mazes multiple_bugtraps shifting_gaps single_bugtrap
 do
-python generate_threat_spp_instances.py --input-path planning-datasets/data/mpd/original/$maze --output-path data/mpd_with_threaten/ --maze-size 32 --mechanism moore
+python generate_threat_spp_instances.py --input-path planning-datasets/data/mpd/original/$maze --output-path data/mpd_with_threat/ --maze-size 32 --mechanism moore
 done
