dd3ace0c4e4f8b1f2c422c697242fc195f34f4ab
BOOMING\guangqian.chen <123@qq.com>
2024-08-08 09:42:38 +0800

    训练配置与脚本

--- scripts/train_threat.py
+++ scripts/train_threat.py
@@ -8,35 +8,37 @@ import pytorch_lightning as pl
 
 sys.path.append(r"./src")
 from module import NeuralAstar
-# from data_utils.maze_data import create_dataloader
-from train_utils.basic_planner import PlannerModule, set_global_seeds
+from data_utils.threaten_data import create_threat_dataloader
+from train_utils.threat_planner import PlannerModule, set_global_seeds
 from pytorch_lightning.callbacks import ModelCheckpoint
 
-@hydra.main(config_path="config", config_name="train")
+@hydra.main(config_path="config", config_name="train_threat")
 def main(config):
 
     # dataloaders
     set_global_seeds(config.seed)
-    # train_loader = create_dataloader(
-    #     config.dataset + ".npz", "train", config.params.batch_size, shuffle=True
-    # )
-    # val_loader = create_dataloader(
-    #     config.dataset + ".npz", "valid", config.params.batch_size, shuffle=False
-    # )
+    train_loader = create_threat_dataloader(
+        config.dataset + ".npz", "train", config.params.batch_size, shuffle=True
+    )
+    val_loader = create_threat_dataloader(
+        config.dataset + ".npz", "valid", config.params.batch_size, shuffle=False
+    )
 
-    # neural_astar = NeuralAstar(
-    #     encoder_input=config.encoder.input,
-    #     encoder_arch=config.encoder.arch,
-    #     encoder_depth=config.encoder.depth,
-    #     learn_obstacles=False,
-    #     Tmax=config.Tmax,
-    # )
+    neural_astar = NeuralAstar(
+        encoder_input=config.encoder.input,
+        encoder_arch=config.encoder.arch,
+        encoder_depth=config.encoder.depth,
+        g_choice=config.astar.g_choice,
+        h_choice=config.astar.h_choice,
+        learn_obstacles=False,
+        Tmax=config.Tmax,
+    )
     checkpoint_callback = ModelCheckpoint(
         monitor="metrics/h_mean", save_weights_only=True, mode="max"
     )
 
-    # module = PlannerModule(neural_astar, config)
-    logdir = f"{config.logdir}/{os.path.basename(config.dataset)}"
+    module = PlannerModule(neural_astar, config)
+    logdir = f"{config.logdir}/{os.path.basename(config.dataset.replace("*", "all_data"))}"
     trainer = pl.Trainer(
         accelerator="gpu" if torch.cuda.is_available() else "cpu",
         log_every_n_steps=1,
@@ -44,7 +46,7 @@ def main(config):
         max_epochs=config.params.num_epochs,
         callbacks=[checkpoint_callback],
     )
-    # trainer.fit(module, train_loader, val_loader)
+    trainer.fit(module, train_loader, val_loader)
 
 
 if __name__ == "__main__":
